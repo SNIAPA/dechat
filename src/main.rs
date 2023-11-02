1663dbad8e0193c6cd5fc25b4615d039383f25c8
SNIAPA <wasilewskihuberts@gmail.com>
2023-11-02 20:15:38 +0100

    switched to yew because idk why i even thought of using rocket

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,24 @@
-#[macro_use] extern crate rocket;
+use yew::prelude::*;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Wsp fags"
+#[function_component]
+fn App() -> Html {
+    let counter = use_state(|| 0);
+    let onclick = {
+        let counter = counter.clone();
+        move |_| {
+            let value = *counter + 1;
+            counter.set(value);
+        }
+    };
+
+    html! {
+        <div>
+            <button {onclick}>{ "+1" }</button>
+            <p>{ *counter }</p>
+        </div>
+    }
 }
 
-#[launch]
-fn rocket() -> _ {
-   rocket::build().mount("/", routes![index])
+fn main() {
+    yew::Renderer::<App>::new().render();
 }
