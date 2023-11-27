62e2acfeb7b66825a9330cb06a0048c81415e828
SNIAPA <wasilewskihuberts@gmail.com>
2023-11-27 05:43:36 +0100

    .

--- src/protocol/node.rs
+++ src/protocol/node.rs
@@ -1,3 +1,5 @@
+use anyhow::{anyhow, Result};
+
 use super::{Body, Message};
 
 #[derive(Debug)]
@@ -7,24 +9,25 @@ impl Node {
     pub fn new() -> Node {
         Node {}
     }
-    pub fn receive(&self, message: Message) -> Option<Message> {
-        match message.body {
-            Body::Echo { msg_id, echo } => Some(Message {
-                src: message.dest,
-                dest: message.src,
-                body: Body::EchoOk {
-                    msg_id: msg_id.clone(),
+    pub fn receive(&self, message: Message) -> Result<Message> {
+        Ok(Message {
+            src: message.dest,
+            dest: message.src,
+            body: match message.body {
+                Body::Echo { msg_id, echo } => Ok(Body::EchoOk {
+                    msg_id: msg_id.clone() + 1,
                     in_reply_to: msg_id,
                     echo,
-                },
-            }),
-            Body::Intit {
-                msg_id,
-                node_id,
-                node_ids,
-            } => Some(Message { src: message.dest, dest: message.src, body: Body::IntitOk { in_reply_to: msg_id } }),
-
-            _ => None,
-        }
+                }),
+                Body::Intit {
+                    msg_id,
+                    node_id: _,
+                    node_ids: _,
+                } => Ok(Body::IntitOk {
+                    in_reply_to: msg_id,
+                }),
+                _ => Err(anyhow!("invalid or not implemented body")),
+            }?,
+        })
     }
 }
--- src/test.rs
+++ src/test.rs
@@ -1,19 +1,30 @@
-use std::io;
+use std::io::{self, stdout, BufRead, Write};
 
 use crate::protocol::{node::Node, Message};
 
 //{  "src": "c1",  "dest": "n1",  "body": {    "type": "echo",    "msg_id": "1",    "echo": "Please echo 35"  }}
+//{"src": "c1", "dest": "n1", "body": {"msg_id": 1, "type": "init", "node_id": "n1", "node_ids": ["n1"]}}
 
 pub fn malestrom_test() {
     let mut buffer = String::new();
     let stdin = io::stdin();
 
     let node = Node::new();
+    let mut stdin_handler = stdin.lock();
+    let mut stdout_handler = stdout().lock();
 
-    stdin.read_line(&mut buffer).unwrap();
-    let parsed_messages = serde_json::from_str::<Message>(&buffer).unwrap();
-    let res = node.receive(parsed_messages);
-    if let Some(res) = res {
-        println!("{}", serde_json::to_string(&res).unwrap())
+    loop {
+        stdin_handler.read_line(&mut buffer).unwrap();
+        dbg!(&buffer);
+        eprint!("req: {}", &buffer);
+        let messages = buffer.trim().split("\n");
+        for message in messages {
+            let parsed_messages = serde_json::from_str::<Message>(&message).unwrap();
+            let res = node.receive(parsed_messages).unwrap();
+            let ans = format!("{}\n", serde_json::to_string(&res).unwrap());
+            eprint!("ans: {}", &ans);
+            stdout_handler.write_all(ans.as_bytes()).unwrap();
+            print!("{}", ans);
+        }
     }
 }
